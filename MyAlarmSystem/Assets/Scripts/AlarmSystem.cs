fcadeed3cbce0efb19cd8ecefa513f24369969c3
RedWave <kurilov6dv@gmail.com>
2022-10-18 19:21:41 +0300

    1.0.1

---
+++ MyAlarmSystem/Assets/Scripts/AlarmSystem.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmSystem : MonoBehaviour
+{
+    [SerializeField] private AudioSource _alarm;
+
+    private bool _inHouse;
+    private float _changeVolume = 0.25f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+            _inHouse = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+            _inHouse = false;
+    }
+
+    private void Update()
+    {
+        if (_inHouse)
+            _alarm.volume += _changeVolume * Time.deltaTime;
+        else
+            _alarm.volume -= _changeVolume * Time.deltaTime;
+        
+    }
+}
