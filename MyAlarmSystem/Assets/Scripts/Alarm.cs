0af6b4bfdc340fe71bf83d428da45292830d5bc9
Lalamee <kurilov6dv@gmail.com>
2022-11-01 16:32:16 +0300

    TryChange

--- MyAlarmSystem/Assets/Scripts/Alarm.cs
+++ MyAlarmSystem/Assets/Scripts/Alarm.cs
@@ -6,29 +6,40 @@ public class Alarm : MonoBehaviour
 {
     [SerializeField] private AudioSource _alarm;
     
-    private float _changeVolume = 0.5f;
+    private float _changeVolumeValue = 0.5f;
     private House _house;
-    
-    private void OnHouseInfiltrated()
+
+    public void OnOffVolume()
     {
-        if (_house.IsInfiltrated)
-        {
-            StopCoroutine(DownVolume());
-            StartCoroutine(UpVolume());
-        }
-        else
-        {
-            StopCoroutine(UpVolume());
-            StartCoroutine(DownVolume());
-        }
+        StartCoroutine(ChangeVolume());
     }
-    
-    private IEnumerator UpVolume()
+
+    private IEnumerator ChangeVolume()
     {
-        _alarm.Play();
-        
-        for (float i = _alarm.volume; i < 1; i += _changeVolume * Time.deltaTime)
+        while (_alarm.volume < 1 || _alarm.volume > 0)
         {
+            if (_house.IsInfiltrated)
+            {
+                _alarm.volume += _changeVolumeValue * Time.deltaTime;
+
+                if (_alarm.volume == 1)
+                {
+                    StopCoroutine(ChangeVolume());
+                }
+            }
+            else
+            {
+                _alarm.volume -= _changeVolumeValue * Time.deltaTime;
+                
+                if (_alarm.volume == 0)
+                {
+                    _alarm.Stop();
+                    StopCoroutine(ChangeVolume());
+                }
+            }
+        }
+        for (float i = _alarm.volume; i < 1; i += _changeVolumeValue * Time.deltaTime)
+        {   
             _alarm.volume =  i;
             yield return null;
         }
@@ -36,7 +47,7 @@ public class Alarm : MonoBehaviour
 
     private IEnumerator DownVolume()
     {
-        for (float i = _alarm.volume; i > 0; i -= _changeVolume * Time.deltaTime)
+        for (float i = _alarm.volume; i > 0; i -= _changeVolumeValue * Time.deltaTime)
         {
             _alarm.volume =  i;
             yield return null;
--- MyAlarmSystem/Assets/Scripts/House.cs
+++ MyAlarmSystem/Assets/Scripts/House.cs
@@ -2,17 +2,24 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
+    private Alarm _alarm;
     public bool IsInfiltrated { get; private set; }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out Player player))
+        {
             IsInfiltrated = true;
+            _alarm.OnOffVolume();
+        }
     }
     
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out Player player))
+        {
             IsInfiltrated = false;
+            _alarm.OnOffVolume();
+        }
     }
 }
